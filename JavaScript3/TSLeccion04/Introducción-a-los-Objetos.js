19937e0e998eacdbe1df65a9e4347ba2cae8800f
José Carmona <92607774+PepCarm@users.noreply.github.com>
2023-05-26 22:07:03 -0300

    Lección 4; Introducción a Objetos en JavaScript

---
+++ JavaScript3/TSLeccion04/Introducción-a-los-Objetos.js
@@ -0,0 +1,16 @@
+let x = 10; //variable de tipo primitiva
+console.log(x.length);
+console.log('Tipos primitivos');
+//Objeto
+let persona = {
+    nombre: 'Carlos',
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad: 30
+    }
+
+console.log('Nombre: '+persona.nombre);
+console.log('Apellido: '+persona.apellido);
+console.log('Email: '+persona.email);
+console.log('Edad:'+persona.edad +' años');
+console.log(persona);
