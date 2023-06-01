62ed55357e12305e890eeac3f6aa0d12479a021f
AleCarb <alecarb36@gmail.com>
2023-06-01 10:48:26 -0300

    mod archivo java

--- Java3/Leccion05/ClaseObject/src/test/testClaseObject.java
+++ Java3/Leccion05/ClaseObject/src/test/testClaseObject.java
@@ -1,5 +1,6 @@
+//una variable del tipo de la clase padre puede almacenar
+//referencias de objeto de la clase hija
 package test;
-
 
 import domain.Empleado;
 import domain.Escritor;
@@ -7,25 +8,30 @@ import domain.TipoEscritura;
 
 public class TestClaseObject {
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado("juan", 2000);
+       Empleado empleado1 = new Empleado("juan", 5000);
         Empleado empleado2 = new Empleado("Juan", 5000);
 
         if (empleado1 == empleado2){
             System.out.println("Tienen la misma referencia en memoria");
-        }else {
-            System.out.println("No tienen la misma referencia en memoria");
+       }
+       else{
+           System.out.println("Tienen distinta referencia en memoria");
         }
 
-        if (empleado1.equals(empleado2)){
-            System.out.println("Los obj son iguales en contenido");
-        }else {
-            System.out.println("Los obj no son iguales en contenido");
+       if(empleado1.equals(empleado2)){
+           System.out.println("Los objetos son iguales en contenido");
+       }
+       else{
+           System.out.println("Los objetos son distintos en contenido");
         }
 
-        if (empleado1.hashCode() == empleado2.hashCode()){
-            System.out.println("el valor en hashcode es igual");
-        }else {
-            System.out.println("el valor en hashcode no es igual");
+       if(empleado1.hashCode() == empleado2.hashCode()){
+           System.out.println("El valor de hash code es igual");
         }
+       else{
+           System.out.println("El valor de hahsCode es diferente");
+       }
+
+
     }
 }
