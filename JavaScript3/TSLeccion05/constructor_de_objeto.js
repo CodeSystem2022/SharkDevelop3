dcad09b169468ebcab4b7baa793f3d2f4139555b
Melisa Riera <112903919+meliriera@users.noreply.github.com>
2023-06-10 12:46:04 -0300

    JS LECCION 5 Agregar métodos al constructor del objeto

--- JavaScript3/TSLeccion05/constructor_de_objeto.js
+++ JavaScript3/TSLeccion05/constructor_de_objeto.js
@@ -2,11 +2,16 @@ function Persona3(nombre, apellido, email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre+ ' '+this.apellido;
+    }
 }
 
 let padre = new Persona3('Gabriel','Valdes','email@gmail.com');
-padre.nombre = 'Luis';
+padre.nombre = 'Luis'; //modificamos el nombre
 console.log(padre);
+console.log(padre.nombreCompleto()); //utilizamos la funcion
 
 let madre = new Persona3('Laura', 'Contreras', 'lcontrera@gmail.com');
-console.log('madre');
+console.log(madre);
+console.log(madre.nombreCompleto());
