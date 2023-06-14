83cfe3395ae8d73a6e85825d6db5cd20a16bda6e
Magali Cardozo <magascard1@gmail.com>
2023-06-14 12:42:32 -0300

    HAGAN LOS COMMITS

--- Java3/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java3/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,11 +1,13 @@
 package test;
 
+import aritmetica.Aritmetica;
+
 public class TestExcepciones {
     public static void main(String[] args) {
         
         int resultado = 0;
         try{
-            resultado = 10/0;
+            resultado = Aritmetica.division(10, 0) ;
         }catch (Exception e){
             System.out.println("Ocurrio un error");
             e.printStackTrace(System.out);
