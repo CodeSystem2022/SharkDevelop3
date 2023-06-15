08e0e5c985163cad9ef6d6d200e25e47153af340
Magali Cardozo <magascard1@gmail.com>
2023-06-15 18:04:34 -0300

    Subo mi parte de la clase 8

--- Java3/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java3/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,16 +1,22 @@
 package test;
 
-import aritmetica.Aritmetica;
+import static aritmetica.Aritmetica.division;
 
 public class TestExcepciones {
     public static void main(String[] args) {
         
         int resultado = 0;
-        try{
-            resultado = Aritmetica.division(10, 0) ;
+        try {
+            resultado = division(10, 2);
+        }catch(ArithmeticException e) {
+            System.out.println(e.getMessage());
         }catch (Exception e){
             System.out.println("Ocurrio un error");
             e.printStackTrace(System.out);
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Se reviso la division entre 0");
         }
         System.out.println("resultado = " + resultado);
 
