1062342f846f19303561e924e28ef8b45b26611d
Melisa Riera <112903919+meliriera@users.noreply.github.com>
2023-06-27 19:58:25 -0300

    Cambio nombre carpetas py
    Organizacion carpetas

---
+++ Python3/Semana 6 (15-05)- Clase 6- Leccion06/manejo_de_transacciones_1_2.py
@@ -0,0 +1,19 @@
+import psycopg2 as bd
+
+conexion = bd.connect(user='postgres', password='admin', host='129.0.0.1', port='5432', database='test_bd')
+
+try:
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    valores = ('Maria', 'Esparza', 'mesparza@gmail.com')
+    cursor.execute(sentencia, valores)
+    conexion.commit()
+    print("Termina la transacción")
+
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
+
+finally:
+    conexion.close()
+
